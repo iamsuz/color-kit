7d86bfe87a0a4a5dd3887e8649d969ea12b22396
Sujit M <s.mahavarkar@gmail.com>
2022-03-07 03:17:01 +0530

    Adds the test cases

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const { generateRandomColor, identifyColor } = require("../src");
+
+const rc = generateRandomColor();
+
+const testColor = "#FF5733";
+const identified = identifyColor(testColor);
+
+const newRandomColor = identifyColor('#FFFFFF')
+console.log(`Identified Color for ${testColor}:`, identified);
+console.log(`Random Color generated ${rc}`, `\n Identified Color for`, newRandomColor)
