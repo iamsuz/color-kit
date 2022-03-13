1bd5b7dcbcadfd4bff63709aad2b9f346d49f8b0
Sujit M <s.mahavarkar@gmail.com>
2022-03-14 03:17:01 +0530

    Updates the test

--- test/test.js
+++ test/test.js
@@ -5,6 +5,6 @@ const rc = generateRandomColor();
 const testColor = "#FF5733";
 const identified = identifyColor(testColor);
 
-const newRandomColor = identifyColor('#FFFFFF')
+const newRandomColor = identifyColor(rc)
 console.log(`Identified Color for ${testColor}:`, identified);
 console.log(`Random Color generated ${rc}`, `\n Identified Color for`, newRandomColor)
