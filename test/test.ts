cb3d01c200ad549e5a725f3ebc34219f3d143020
Sujit M <s.mahavarkar@gmail.com>
2023-09-12 03:17:01 +0530

    Adds the test for typescript

---
+++ test/test.ts
@@ -0,0 +1,20 @@
+import { identifyColor, hexToRgb } from "../src/identifyColor";
+import { generateRandomColor } from "../src/randomColor";
+import { Color } from "../statics/colors";
+
+// Using identifyColor
+const colorInfo = identifyColor("#FF5733");
+console.log(colorInfo.colorName); // "Closest Color Name"
+console.log(colorInfo.distance); // Distance if not an exact match
+
+// Using hexToRgb
+const rgb = hexToRgb("#FF5733");
+console.log(rgb); // [255, 87, 51]
+
+// Using generateRandomColor
+const randomColor = generateRandomColor();
+console.log(randomColor); // "#A1B2C3"
+
+// Using Color interface
+const exampleColor: Color = { hex: "#FFFFFF", name: "White" };
+console.log(exampleColor);
