a19b87bb217a76d550ce89907a7c8a7a06c6ab14
Sujit M <s.mahavarkar@gmail.com>
2023-11-12 03:17:01 +0530

    Adds the test for typescript

--- test/test.js
+++ test/test.js
@@ -1,10 +1,17 @@
-const { generateRandomColor, identifyColor } = require("../src");
-
-const rc = generateRandomColor();
-
-const testColor = "#FF5733";
-const identified = identifyColor(testColor);
-
-const newRandomColor = identifyColor(rc)
-console.log(`Identified Color for ${testColor}:`, identified);
-console.log(`Random Color generated ${rc}`, `\n Identified Color for`, newRandomColor)
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var identifyColor_1 = require("../src/identifyColor");
+var randomColor_1 = require("../src/randomColor");
+// Using identifyColor
+var colorInfo = (0, identifyColor_1.identifyColor)("#FF5733");
+console.log(colorInfo); // "Closest Color Name"
+console.log(colorInfo.distance); // Distance if not an exact match
+// Using hexToRgb
+var rgb = (0, identifyColor_1.hexToRgb)("#FF5733");
+console.log(rgb); // [255, 87, 51]
+// Using generateRandomColor
+var randomColor = (0, randomColor_1.generateRandomColor)();
+console.log(randomColor); // "#A1B2C3"
+// Using Color interface
+var exampleColor = { hex: "#FFFFFF", name: "White" };
+console.log(exampleColor);
