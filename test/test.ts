618b1e00cbae6baedc6dd624115380dd50f362c0
Sujit M <s.mahavarkar@gmail.com>
2023-11-18 03:17:01 +0530

    Adds the test for typescript

--- test/test.ts
+++ test/test.ts
@@ -5,12 +5,13 @@ import { Color } from "../statics/colors";
 // Using identifyColor
 const colorInfo = identifyColor("#FF5733");
 
-console.log(colorInfo); // "Closest Color Name"
+console.log(colorInfo.colorName); // "Closest Color Name"
 console.log(colorInfo); // Distance if not an exact match
 
 // Using hexToRgb
 const rgb = hexToRgb("#FF5733");
-console.log(rgb); // [255, 87, 51]
+
+console.log({ rgb }); // [255, 87, 51]
 
 // Using generateRandomColor
 const randomColor = generateRandomColor();
