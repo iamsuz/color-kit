6160b2973334d74b8423d753222717c5658c7942
Sujit M <s.mahavarkar@gmail.com>
2023-12-20 03:17:01 +0530

    Adds the test for typescript

--- test/test.ts
+++ test/test.ts
@@ -11,11 +11,11 @@ console.log(colorInfo.distance); // Distance if not an exact match
 // Using hexToRgb
 const rgb = hexToRgb("#FF5733");
 
-console.log(rgb); // [255, 87, 51]
+console.log({ rgb }); // [255, 87, 51]
 
 // Using generateRandomColor
 const randomColor = generateRandomColor();
-console.log(randomColor); // "#A1B2C3"
+console.log({ randomColor }); // "#A1B2C3"
 
 // Using Color interface
 const exampleColor: Color = { hex: "#FFFFFF", name: "White" };
