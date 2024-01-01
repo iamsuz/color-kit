a75b802237720dd403412ea61040ee18de8ec410
Sujit M <s.mahavarkar@gmail.com>
2024-01-02 03:17:01 +0530

    Adds the test for typescript

--- test/test.ts
+++ test/test.ts
@@ -15,8 +15,8 @@ console.log({ rgb }); // [255, 87, 51]
 
 // Using generateRandomColor
 const randomColor = generateRandomColor();
-// console.log({ randomColor }); // "#A1B2C3"
+console.log({ randomColor }); // "#A1B2C3"
 
 // Using Color interface
 const exampleColor: Color = { hex: "#FFFFFF", name: "White" };
-// console.log({ exampleColor });
+console.log({ exampleColor });
